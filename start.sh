24a698d1d6a62108b05b50dc54ffefe2d6e1c461
xinghao <Xinghao@airarena.net>
2013-12-12 17:29:08 +1100

    correct script for production

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 #java -X8192msm -Xmx12288m -Dsolr.solr.home=/SOLR -DSTOP.PORT=8079 -DSTOP.KEY=mysecret -jar /SOLR/SolrServer/current/start.jar
-#java -Dsolr.data.dir=/SOLR/data -DSTOP.PORT=8079 -DSTOP.KEY=mysecret -jar /SOLR/SolrServer/current/start.jar
-java -DSTOP.PORT=8079 -DSTOP.KEY=mysecret -jar start.jar
+java -Dsolr.data.dir=/SOLR/data -DSTOP.PORT=8079 -DSTOP.KEY=mysecret -jar /SOLR/SolrServer/current/start.jar
+#java -DSTOP.PORT=8079 -DSTOP.KEY=mysecret -jar start.jar
