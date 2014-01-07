14720a65269ceb36439919f9e69dc898cccd893c
xinghao <Xinghao@airarena.net>
2014-01-07 16:42:12 +1100

    disable logging

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 #java -X8192msm -Xmx12288m -Dsolr.solr.home=/SOLR -DSTOP.PORT=8079 -DSTOP.KEY=mysecret -jar /SOLR/SolrServer/current/start.jar
-java -Dsolr.data.dir=/SOLR/data -DSTOP.PORT=8079 -DSTOP.KEY=mysecret -jar /SOLR/SolrServer/current/start.jar
+java -Dsolr.data.dir=/SOLR/data -Djava.util.logging.config.file=/SOLR/SolrServer/current/solr/pfparty/conf/logging.properties -DSTOP.PORT=8079 -DSTOP.KEY=mysecret -jar /SOLR/SolrServer/current/start.jar
 #java -DSTOP.PORT=8079 -DSTOP.KEY=mysecret -jar start.jar
