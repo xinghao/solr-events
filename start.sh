999c14349cc8f230ca212f56f331bae9e24e814d
xinghao <Xinghao@airarena.net>
2014-08-19 21:43:44 +1000

    fix solr home

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 #java -X8192msm -Xmx12288m -Dsolr.solr.home=/SOLR -DSTOP.PORT=8079 -DSTOP.KEY=mysecret -jar /SOLR/SolrServer/current/start.jar
-java -Djetty.home=/SOLR/SolrServer/current/solr -Dsolr.solr.home=/SOLR/SolrServer/current -Dsolr.data.dir=/SOLR/data -DSTOP.PORT=8079 -DSTOP.KEY=mysecret -jar /SOLR/SolrServer/current/start.jar
+java -Djetty.home=/SOLR/SolrServer/current/solr -Dsolr.solr.home=/SOLR/SolrServer/current/solr -Dsolr.data.dir=/SOLR/data -DSTOP.PORT=8079 -DSTOP.KEY=mysecret -jar /SOLR/SolrServer/current/start.jar
 #java -DSTOP.PORT=8079 -DSTOP.KEY=mysecret -Dsolr.data.dir=/Users/xinghao/Documents/rails/solr-4.6.0-weedfinda/data -jar start.jar
